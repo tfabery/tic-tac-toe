f1d8f84c1edbfa1ff7acfcfc9aeb9ca635864faa
Bhawani Parajuli and Tony Fabery <bhawani@>
2016-06-16 13:15:38 -0700

    It will execute some AI actions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 //=======================backend=========================
-function Player(num, mark) {
+function Player(num, mark, player) {
   this.mark = mark;
   this.num = num;
   this.score = 0;
+  this.player = player;
   // var markArray = ['X', 'O'];
   // for (i = 0; i < markArray.length; i ++) {
   //   this.mark[i] = markArray[i];
@@ -13,6 +14,12 @@ Player.prototype.select = function(x, y) {
   if (board.spaces[x][y].markBy === null) {
     board.spaces[x][y].markBy = this.mark;
     return true;
+  }
+  else if (this.player === 'ai') {
+    return false;
+  }
+  else if (this.player === 'human') {
+    return false;
   }
   else {
     alert('Selected space is already marked select an unmarked space');
@@ -94,19 +101,50 @@ BoardInterface.prototype.renderBoard = function() {
 BoardInterface.prototype.render = function() {
   $(this.selector).empty().append(this.renderBoard());
   $('.btn').hide();
+};
+
+function renderWinnermsg () {
+  $('.newBoard').empty().append('<h1>' + player.num + ' wins!!</h1>');
 }
 
 function clicked() {
   for (var y = this.board.height; y >= 1; y -= 1) {
     for (var x = 1; x <= this.board.width; x += 1) {
+      if (player.player === 'ai') {
+        var xCordAi = selectRandom();
+        var yCordAi = selectRandom();
+        var selectedAi = player.select(xCordAi, yCordAi);
+
+        if (selectedAi) {
+          $('<span class="glyphicon glyphicon-remove"></span>').appendTo($('#y' + yCordAi + ' #x' + xCordAi));
+          player.score += board.spaces[xCordAi][yCordAi].indicator;
+
+          if (board.winCheck()) {
+            alert('Player 1 wins');
+            homePage();
+          }
+          else if (board.tie() === 9) {
+            alert('Draw....');
+            homePage();
+          }
+          else {
+            player = player2
+          }
+        }
+        clicked();
+      }
+
+
       $('#y' + y + ' #x' + x).click(function() {
         var xCord = parseInt($(this).attr('id').substring(1));
         var yCord = parseInt($(this).parent().attr('id').substring(1));
         var selected = player.select(xCord, yCord);
+
         if (selected) {
           if (player.mark === 'X') {
             $('<span class="glyphicon glyphicon-remove"></span>').appendTo($(this));
             player.score += board.spaces[xCord][yCord].indicator;
+
             if (board.winCheck()) {
               alert('Player 1 wins');
               homePage();
@@ -122,6 +160,7 @@ function clicked() {
           else if (player.mark === 'O') {
             $('<span class="glyphicon glyphicon-unchecked"></span>').appendTo($(this));
             player.score += board.spaces[xCord][yCord].indicator;
+
             if (board.winCheck()) {
               alert('Player 2 wins');
               homePage();
@@ -131,7 +170,10 @@ function clicked() {
               homePage();
             }
             else {
+              xCord = undefined;
+              yCord = undefined;
               player = player1
+              clicked();
             }
           }
         }
@@ -139,18 +181,35 @@ function clicked() {
     };
   };
 };
+
 function homePage() {
   $(".newBoard").empty();
   $(".btn").show();
-}
+};
+
 function startNew() {
-  player1 = new Player(1, 'X');
-  player2 = new Player(2, 'O');
+  $
+  player1 = new Player(1, 'X', 'human');
+  player2 = new Player(2, 'O', 'human');
   player = player1;
   board = new Board();
   var ui = new BoardInterface(board, '.newBoard');
   ui.render();
   clicked();
+};
+
+function startAi() {
+  player1 = new Player(1, 'X', 'ai');
+  player2 = new Player(2, 'O', 'human');
+  player = player1;
+  board = new Board();
+  var ui = new BoardInterface(board, '.newBoard');
+  ui.render();
+  clicked();
+}
+
+function selectRandom(){
+  return Math.floor(Math.random() * (3))+1;
 }
 //=======================frontend========================
 var player1;
@@ -163,7 +222,7 @@ $(function() {
   });
 
   $("#vAi").click(function() {
-    startNew();
+    startAi();
   });
 
 });
